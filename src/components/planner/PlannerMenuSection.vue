ba937b61a022e85b8aebd3b96fe0baf5cebb3855
azadix <bartosz@madalski.pl>
2026-05-02 12:16:15 +0200

    Dictionary and known issue list update

--- src/components/planner/PlannerMenuSection.vue
+++ src/components/planner/PlannerMenuSection.vue
@@ -65,6 +65,7 @@ import {
             <li>- Many skills are missing descriptions</li>
             <li>- Skill damage and most of mana cost formulas are not implemented</li>
             <li>- War Spirit synergies based on devotion are not filled in</li>
+            <li>- Incorrect calculation for Warmth passive</li>
           </ul>
         </div>
       </div>
